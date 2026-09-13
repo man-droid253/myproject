007b03b250fc31b3f7055d4045d1446397ed858d
man-droid253 <masonfusire@gmail.com>
2026-09-13 17:31:59 +0100

    added a 404 and 505 page to handle errors

--- app.py
+++ app.py
@@ -236,5 +236,14 @@ def edit_project(project_id):
     return render_template('edit_project.html', project=project)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
